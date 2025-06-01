66fe0300d4c5e9b31d8d394c2d102d8957b2f317
Ibrahim Yusuf <hireyusufibrahim@gmail.com>
2025-06-01 16:06:24 +0100

    Wrote Why choose us components

--- client/src/app/LandingPage/page.tsx
+++ client/src/app/LandingPage/page.tsx
@@ -1,11 +1,13 @@
 
 import { Hero } from "./components/Hero";
+import { WhyChooseUs } from "./components/WhyChoosUs";
 
 export default function LandingPage() {
   return (
     <div className="scroll-smooth">
         <main>
             <Hero />
+            <WhyChooseUs />
         </main>
     </div>
   );
